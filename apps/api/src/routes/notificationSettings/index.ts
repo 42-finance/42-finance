8b44f33b645414ae0c0186ffb7e40356988e05ca
Jeff Swarts <jeffswarts@hotmail.com>
2024-09-21 05:27:37 -0600

    Fix previous app versions notification settings

--- apps/api/src/routes/notificationSettings/index.ts
+++ apps/api/src/routes/notificationSettings/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 
+import { createNotificationSetting } from './createNotificationSetting'
+import { deleteNotificationSetting } from './deleteNotificationSetting'
 import { getNotificationSettings } from './getNotificationSettings'
 import { updateNotificationSetting } from './updateNotificationSetting'
 
 const notificationSettingsRouter = express.Router()
 notificationSettingsRouter.get('/', getNotificationSettings)
+notificationSettingsRouter.post('/:type', createNotificationSetting)
+notificationSettingsRouter.delete('/:type', deleteNotificationSetting)
 notificationSettingsRouter.patch('/:type', updateNotificationSetting)
 
 export { notificationSettingsRouter }
