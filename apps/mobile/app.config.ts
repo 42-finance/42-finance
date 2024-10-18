23c01774ecbf2949b5263b3079deb8e95bb4f1bf
jeffswarts <jeff@42f.io>
2024-10-18 16:23:40 -0600

    Increment mobile version

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -36,7 +36,7 @@ export default (): ExpoConfig => {
     name: appName,
     slug: 'fortytwofinance',
     owner: '42-finance',
-    version: '1.1.1',
+    version: '1.1.2',
     orientation: 'portrait',
     icon: './src/assets/images/icon.png',
     scheme: 'fortytwofinance',
