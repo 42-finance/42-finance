cfa76c700fab1d364a3fdd12397c266032f09d5c
Jeff Swarts <jeffswarts@hotmail.com>
2024-10-18 17:02:38 -0600

    Dashboard widgets fix

--- apps/api/src/routes/dashboardWidget/getDashboardWidgets.ts
+++ apps/api/src/routes/dashboardWidget/getDashboardWidgets.ts
@@ -1,17 +1,23 @@
 import { DashboardWidget, dataSource } from 'database'
 import { Request, Response } from 'express'
+import semver from 'semver'
+import { DashboardWidgetType } from 'shared-types'
 
 import { HTTPResponseBody } from '../../models/http/httpResponseBody'
 
 export const getDashboardWidgets = async (request: Request<{}, {}, {}, {}>, response: Response<HTTPResponseBody>) => {
-  const { userId } = request
+  const { userId, appVersion } = request
 
-  const dashboardWidgets = await dataSource
+  let dashboardWidgets = await dataSource
     .getRepository(DashboardWidget)
     .createQueryBuilder('dashboardWidget')
     .where('dashboardWidget.userId = :userId', { userId })
     .addOrderBy('dashboardWidget.order')
     .getMany()
+
+  if (appVersion == null || semver.lt(appVersion, '1.1.1')) {
+    dashboardWidgets = dashboardWidgets.filter((d) => d.type !== DashboardWidgetType.DateSpending)
+  }
 
   return response.send({
     errors: [],
